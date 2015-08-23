11320aaa70286a24df747c15370fce441f9e49d5
jmpelham <lilserious0083@gmail.com>
2015-08-22 23:47:28 -0400

    Adding all files

---
+++ js/main.js
@@ -0,0 +1,27 @@
+//angular.module('jpSite', function () {
+//	.controller
+//});
+
+var app = angular.module('jpSite', ['ngRoute']);
+
+app.controller('MainCtrl', ['$route', '$routeParams', '$location',
+  function($route, $routeParams, $location) {
+    this.$route = $route;
+    this.$location = $location;
+    this.$routeParams = $routeParams;
+}]);
+
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+$routeProvider
+// Home
+.when("/", {templateUrl: "html/home.html"})
+// Music
+.when("/music", {templateUrl: "html/music.html"})
+// Blog
+.when("/blog", {templateUrl: "html/blog.html"})
+// Videos
+.when("/videos", {templateUrl: "html/videos.html"})
+// Contact
+.when("/contact", {templateUrl: "html/contact.html"})
+
+}]);
