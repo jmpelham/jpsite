6cfba54527cfbb12935e9c7a66b02a9982f940cc
unknown <lilserious0083@gmail.com>
2015-08-28 23:15:28 -0400

    Got JSON working with SCE service

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ app.controller('MainCtrl', ['$route', '$routeParams', '$location',
 }]);
 
 // Routing
-app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+app.config(['$routeProvider', '$locationProvider', '$sceDelegateProvider', function ($routeProvider, $locationProvider, $sceDelegateProvider) {
 $routeProvider
 // Home
 .when("/", {templateUrl: "html/home.html"})
@@ -28,24 +28,15 @@ $routeProvider
 // Contact
 .when("/contact", {templateUrl: "html/contact.html"})
 
-}]);
+$sceDelegateProvider.resourceUrlWhitelist(
+	[
+     'self',
+     '*://www.youtube.com/**',
+     '*://w.soundcloud.com/**'
+	]
+);
 
-// File Reader
-function readFile (filePath) {
-	if (FileReader) {
-		if (window.File && window.FileReader && window.FileList && window.Blob) {
-		  var reader = new FileReader();
-		  
-		  reader.onload = function(e) {
-		  	return reader.result;
-		  }
-		  
-		  reader.readAsText(filePath);
-		} else {
-		  alert('Whoops. Your browser not supported.');
-		}
-	}
-}
+}]);
 
 //Div Creation
 app.controller('musicPostCtrl', function ($scope, $sce, $http) {
@@ -58,28 +49,5 @@ app.controller('musicPostCtrl', function ($scope, $sce, $http) {
 	    error(function(data, status, headers, config) {
 	      alert("Error: Could not retrieve data.")
 	    });
-    //$scope.posts.musicPosts = readFile('data/musicPosts.txt');
-
-    /*[
-    	{
-    		title: 'First Post',
-    		date: 'Jan 1',
-    		description: 'Fist music post.',
-    		embedLink: $sce.trustAsResourceUrl('https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/220882008&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false')
-    	},
-    	{
-    		title: 'Second Post',
-    		date: 'Feb 2',
-    		description: 'Second music post.',
-    		embedLink: $sce.trustAsResourceUrl('https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/163063336&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false')
-    	},
-    	{
-    		title: 'Third Post',
-    		date: 'Oct 13',
-    		description: 'Third music post.',
-    		embedLink: $sce.trustAsResourceUrl('https://www.youtube.com/embed/GkXLLaBTGZI?rel=0')
-    	}
-    ]*/
-
 });
 
